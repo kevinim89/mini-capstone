48a0e6311444770ea64d2de8ea7c507fa8ecfd93
dani <dani.zaghian@gmail.com>
2021-08-25 11:08:24 -0700

    adds a bunch of useless routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,19 @@ class ProductsController < ApplicationController
     render json: products.as_json
   end
 
+  def first_product
+    product = Product.first
+    render json: product.as_json
+  end
+
+  def fifth_product
+    product = Product.fifth
+    render json: product.as_json
+  end
+
+  def sixth_product
+    product = Product.find(6)
+    render json: product.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "all_products"
+  get "/first_product", controller: "products", action: "first_product"
+  get "/fifth_product", controller: "products", action: "fifth_product"
+  get "/sixth_product", controller: "products", action: "sixth_product"
 end
