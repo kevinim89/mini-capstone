46b0b86560475a60248c2d89203deb9ed3330e14
dani <dani.zaghian@gmail.com>
2021-08-31 12:08:25 -0700

    adds migrations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,7 @@ class ProductsController < ApplicationController
       price: params[:price],
       image_url: params[:image_url],
       description: params[:description],
+      inventory: params[:inventory],
     )
     product.save
     render json: product
@@ -26,6 +27,7 @@ class ProductsController < ApplicationController
     product.price = params[:price] || product.price
     product.image_url = params[:image_url] || product.image_url
     product.description = params[:description] || product.description
+    product.inventory = params[:inventory] || product.inventory
     product.save
     render json: product
   end
