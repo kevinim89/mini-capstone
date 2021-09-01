2e1d6a3c90848a95e3b44c047b7998ab31878955
dani <dani.zaghian@gmail.com>
2021-09-01 14:03:08 -0700

    adds association methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ApplicationRecord
 
+  # association method
+  belongs_to :supplier #returns a hash of supplier data
+
+  # def supplier
+  #   Supplier.find(supplier_id)
+  # end
+
+  # model methods
   def is_discounted?
     price < 10
   end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,9 @@
 class Supplier < ApplicationRecord
+  # association method
+  has_many :products # array of product hashes
+
+  # def products
+  #   Product.where(supplier_id: id)
+  # end
 end
+
