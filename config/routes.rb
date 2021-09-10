3691c2235c863d4e0115ca21190ec8ebe5ba69d6
dani <dani.zaghian@gmail.com>
2021-09-10 13:56:37 -0700

    adds orders create, index, and show

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   get "/products/:id" => "products#show"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
+
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+
+  get "/orders" => "orders#index"
+  post "/orders" => "orders#create"
+  get "/orders/:id" => "orders#show"
 end
